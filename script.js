75f448d8128b1e96bdf5450be9f6cbe3d6fabe14
pc <www.mmhmdmm83@gmail.com>
2021-12-17 15:30:15 +0330

    fix menu link

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ const menu = document.querySelector('.menu');
 
 const hamberger = document.querySelector('.hamberger');
 
+let linkItem = document.querySelectorAll('.link-item')
+
 hamberger.addEventListener('click', function () {
     menu.classList.toggle('hide');
     hamberger.classList.toggle('rotate')
 });
+
+linkItem.forEach(function (link) {
+    link.addEventListener('click' , function () {
+        menu.classList.remove('hide')
+    })
+})
