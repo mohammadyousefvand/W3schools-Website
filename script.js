772a9630dbb2e7ffd6cfe35916f4850627e7f6f5
pc <www.mmhmdmm83@gmail.com>
2021-12-22 21:39:33 +0330

    set scroll top

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const menu = document.querySelector('.menu');
-
 const hamberger = document.querySelector('.hamberger');
+const linkItem = document.querySelectorAll('.link-item')
+const goUpScroll = document.querySelector('.go-up')
 
-let linkItem = document.querySelectorAll('.link-item')
+// function
+
+function goUpScrollHanler() {
+    window.scrollTo(0, 0)
+}
 
 hamberger.addEventListener('click', function () {
     menu.classList.toggle('hide');
@@ -10,7 +15,9 @@ hamberger.addEventListener('click', function () {
 });
 
 linkItem.forEach(function (link) {
-    link.addEventListener('click' , function () {
+    link.addEventListener('click', function () {
         menu.classList.remove('hide')
     })
 })
+
+goUpScroll.addEventListener('click', goUpScrollHanler)
